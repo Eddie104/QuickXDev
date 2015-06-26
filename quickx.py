dd726263a8b10a07b8684ef3f5312ccfa6c1551c
hongjie <blueheart104@tom.com>
2015-06-26 13:00:54 +0800

    加了些小功能

--- quickx.py
+++ quickx.py
@@ -15,6 +15,7 @@ import subprocess
 import sys
 import time
 import codecs
+import socket
 try:
     import helper
     import rebuild
@@ -28,10 +29,25 @@ TEMP_PATH=""
 # [wordsArr,showFunc,path,lineNum,type] type=0 user, 1 lua, 2 cocos2dx
 DEFINITION_LIST=[]
 USER_DEFINITION_LIST=[]
+
 luaTemplate="""--
 -- Author: ${author}
 -- Date: ${date}
 --
+"""
+
+luaClassTemplate="""--
+-- Author: ${author}
+-- Date: ${date}
+--
+
+local ${fileName} = class("${fileName}")
+
+function ${fileName}:ctor()
+
+end
+
+return ${fileName}
 """
 
 # init plugin,load definitions
@@ -48,9 +64,9 @@ def init():
 def checkQuickxRoot():
     # quick_cocos2dx_root
     settings = helper.loadSettings("QuickXDev")
-    quick_cocos2dx_root = settings.get("quick_cocos2dx_root", "")
+    quick_cocos2dx_root = settings.get("quick_3_cocos2dx_root", "")
     if len(quick_cocos2dx_root)==0:
-        sublime.error_message("quick_cocos2dx_root no set")
+        sublime.error_message("quick_3_cocos2dx_root no set")
         return False
     return quick_cocos2dx_root
 
@@ -64,6 +80,82 @@ def checkQuickxRoot():
 #     return cocos2dx_root
 
 process=None
+# def runWithPlayer(srcDir):
+#     global process
+#     # root
+#     quick_cocos2dx_root = checkQuickxRoot()
+#     if not quick_cocos2dx_root:
+#         return
+#     # player path for platform
+#     playerPath=""
+#     if sublime.platform()=="osx":
+#         playerPath=quick_cocos2dx_root+"/player3.app/Contents/MacOS/player3"
+#     elif sublime.platform()=="windows":
+#         # playerPath=quick_cocos2dx_root+"/quick/player/win32/player3.exe"
+#         playerPath = quick_cocos2dx_root+"/tools/simulator/runtime/win32/simulator.exe"
+#     if playerPath=="" or not os.path.exists(playerPath):
+#         sublime.error_message("player no exists")
+#         return
+#     args=[playerPath]
+#     # param
+#     path=srcDir
+#     arr=os.path.split(path)
+#     workdir=arr[0]
+#     srcDirName=arr[1]
+#     args.append("-workdir")
+#     args.append(workdir)
+#     args.append("-file")
+#     args.append(srcDirName+"/main.lua")
+#     configPath=path+"/config.lua"
+#     if os.path.exists(configPath):
+#         f=codecs.open(configPath,"r","utf-8")
+#         width=640
+#         height=960
+#         while True:
+#             line=f.readline()
+#             if line:
+#                 # debug
+#                 m=re.match("^DEBUG\s*=\s*(\d+)",line)
+#                 if m:
+#                     debug=m.group(1)
+#                     if debug=="0":
+#                         args.append("-disable-write-debug-log")
+#                         args.append("-disable-console")
+#                     elif debug=="1":
+#                         args.append("-disable-write-debug-log")
+#                         args.append("-console")                            
+#                     else:
+#                         args.append("-write-debug-log")
+#                         args.append("-console")
+#                 # resolution
+#                 m=re.match("^CONFIG_SCREEN_WIDTH\s*=\s*(\d+)",line)
+#                 if m:
+#                     width=m.group(1)
+#                 m=re.match("^CONFIG_SCREEN_HEIGHT\s*=\s*(\d+)",line)
+#                 if m:
+#                     height=m.group(1)
+
+#                 m = re.match("^CONFIG_SCREEN_ORIENTATION\s*=\s*\"portrait\"",line)
+#                 if m:
+#                     args.append("-portrait")
+#                 m = re.match("^CONFIG_SCREEN_ORIENTATION\s*=\s*\"landscape\"",line)
+#                 if m:
+#                     args.append("-landscape")
+#             else:
+#                 break
+#         f.close()
+#         args.append("-size")
+#         args.append(width+"x"+height)
+#     if process:
+#         try:
+#             process.terminate()
+#         except Exception:
+#             pass
+#     if sublime.platform()=="osx":
+#         process=subprocess.Popen(args)
+#     elif sublime.platform()=="windows":
+#         process=subprocess.Popen(args)
+
 def runWithPlayer(srcDir):
     global process
     # root
@@ -76,6 +168,7 @@ def runWithPlayer(srcDir):
         playerPath=quick_cocos2dx_root+"/player3.app/Contents/MacOS/player3"
     elif sublime.platform()=="windows":
         playerPath=quick_cocos2dx_root+"/quick/player/win32/player3.exe"
+        # playerPath = quick_cocos2dx_root+"/tools/simulator/runtime/win32/simulator.exe"
     if playerPath=="" or not os.path.exists(playerPath):
         sublime.error_message("player no exists")
         return
@@ -103,25 +196,29 @@ def runWithPlayer(srcDir):
                     debug=m.group(1)
                     if debug=="0":
                         args.append("-disable-write-debug-log")
-                        args.append("-disable-console")
+                        args.append("debug.log")
+                        args.append("-console")
+                        args.append("disenable")
                     elif debug=="1":
-                        args.append("-disable-write-debug-log")
-                        args.append("-console")                            
+                        args.append("-write-debug-log")
+                        args.append("debug.log")
+                        args.append("-console")
+                        args.append("enable")
                     else:
                         args.append("-write-debug-log")
                         args.append("-console")
-                # resolution
-                m=re.match("^CONFIG_SCREEN_WIDTH\s*=\s*(\d+)",line)
+                        args.append("enable")
+
+                m = re.match("^CONFIG_SCREEN_ORIENTATION\s*=\s*\"portrait\"",line)
                 if m:
-                    width=m.group(1)
-                m=re.match("^CONFIG_SCREEN_HEIGHT\s*=\s*(\d+)",line)
-                if m:
-                    height=m.group(1)
+                    args.append("-portrait")
+                else:
+                    m = re.match("^CONFIG_SCREEN_ORIENTATION\s*=\s*\"landscape\"",line)
+                    if m:
+                        args.append("-landscape")
             else:
                 break
         f.close()
-        args.append("-size")
-        args.append(width+"x"+height)
     if process:
         try:
             process.terminate()
@@ -131,6 +228,7 @@ def runWithPlayer(srcDir):
         process=subprocess.Popen(args)
     elif sublime.platform()=="windows":
         process=subprocess.Popen(args)
+    print(args)
     
 
 class LuaNewFileCommand(sublime_plugin.WindowCommand):
@@ -148,7 +246,11 @@ class LuaNewFileCommand(sublime_plugin.WindowCommand):
         if os.path.exists(filePath):
             sublime.error_message("Unable to create file, file exists.")
         else:
-            code = luaTemplate
+            if name[0].islower():
+                code = luaTemplate
+            else:
+                code = luaClassTemplate
+                code = code.replace("${fileName}", name.replace(".lua", ""))
             # add attribute
             settings = helper.loadSettings("QuickXDev")
             format = settings.get("date_format", "%Y-%m-%d %H:%M:%S")
@@ -156,6 +258,7 @@ class LuaNewFileCommand(sublime_plugin.WindowCommand):
             code = code.replace("${date}", date)
             author=settings.get("author", "Your Name")
             code = code.replace("${author}", author)
+
             # save
             helper.writeFile(filePath, code)
             v=sublime.active_window().open_file(filePath)
@@ -204,6 +307,31 @@ class QuickxRunWithPlayerByFileCommand(sublime_plugin.TextCommand):
     def is_visible(self):
         return self.is_enabled()
 
+
+class QuickxCreateGetterAndSetterCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel=self.view.substr(self.view.sel()[0])
+        if len(sel)==0:
+            # extend to the `word` under cursor
+            sel=self.view.substr(self.view.word(self.view.sel()[0]))
+        if sel[0] == '_':
+            lineEnd = self.view.full_line(self.view.sel()[0]).end()
+            nextLineEnd = self.view.full_line(lineEnd + 1).end()
+            nextLine = self.view.substr(sublime.Region(lineEnd, nextLineEnd))
+
+            endCompile = re.compile('end')
+
+            match = endCompile.match(nextLine)
+            while not match:
+                lineEnd = nextLineEnd
+                nextLineEnd = self.view.full_line(lineEnd + 1).end()
+                nextLine = self.view.substr(sublime.Region(lineEnd, nextLineEnd))
+                match = endCompile.match(nextLine)
+
+            funName = sel[1:]
+            fileName = self.view.file_name().split("\\")[-1].split('.')[0]
+            text = 'function %s:%s(val)\n\tif val then\n\t\tself.%s = val\n\t\treturn self\n\tend\n\treturn self.%s\nend\n\n' % (fileName, funName, sel, sel)
+            self.view.insert(edit, nextLineEnd + 1, text)
 
 class QuickxGotoDefinitionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -421,25 +549,118 @@ class QuickxListener(sublime_plugin.EventListener):
             return
         # rebuild user definition
         curTime=time.time()
-        if curTime-self.lastTime<2:
+        if curTime-self.lastTime<10:
             return
         self.lastTime=curTime
-        a=rebuild.rebuildSingle(filename,TEMP_PATH)
-        arr=a[0]
-        path=a[1] 
-        # remove prev
-        global USER_DEFINITION_LIST
-        for i in range(len(USER_DEFINITION_LIST)-1,0,-1):
-            item=USER_DEFINITION_LIST[i]
-            if item[2]==path:
-                USER_DEFINITION_LIST.remove(item)
-        USER_DEFINITION_LIST.extend(arr)
-        path=os.path.join(TEMP_PATH, "user_definition.json")
-        data=json.dumps(USER_DEFINITION_LIST)
-        if not os.path.exists(TEMP_PATH):
-            os.makedirs(TEMP_PATH)
-        helper.writeFile(path,data)
-        sublime.status_message("Current file definition rebuild complete!")
+        
+        # a=rebuild.rebuildSingle(filename,TEMP_PATH)
+        # arr=a[0]
+        # path=a[1] 
+        # # remove prev
+        # global USER_DEFINITION_LIST
+        # for i in range(len(USER_DEFINITION_LIST)-1,0,-1):
+        #     item=USER_DEFINITION_LIST[i]
+        #     if item[2]==path:
+        #         USER_DEFINITION_LIST.remove(item)
+        # USER_DEFINITION_LIST.extend(arr)
+        # path=os.path.join(TEMP_PATH, "user_definition.json")
+        # data=json.dumps(USER_DEFINITION_LIST)
+        # if not os.path.exists(TEMP_PATH):
+        #     os.makedirs(TEMP_PATH)
+        # helper.writeFile(path,data)
+        # sublime.status_message("Current file definition rebuild complete!")
+
+        settings = helper.loadSettings("QuickXDev")
+        if settings.get("update", ""):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                sock.connect(('localhost', 3630))
+                sock.send(filename.encode())
+            except Exception as err:
+                print("recv", err)
+            finally:
+                sock.close()
+
+    def on_load(self, view):
+        if view.file_name().find("debug.log") != -1:
+            view.set_syntax_file("Packages/Java/Java.tmLanguage")
+
+class QuickxUpdateLuaCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        settings = helper.loadSettings("QuickXDev")
+        if settings.get("update", ""):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                sock.connect(('localhost', 3630))
+                sock.send('updateLua'.encode())
+            except Exception as err:
+                print("recv", err)
+            finally:
+                sock.close()
+        
+    def is_enabled(self):
+        return helper.checkFileExt(self.view.file_name(),"lua")
+
+    def is_visible(self):
+        return self.is_enabled()
+
+class MySpecialDoubleclickCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if not self.view.file_name():
+            return
+        file_path, file_name = os.path.split(self.view.file_name())
+        if file_name == "debug.log":
+            for region in self.view.sel():
+                if not region.empty():
+                    line = self.view.substr(self.view.line(region.a))
+                    break
+
+            # print("line:",line)
+            re, filename = self.parseLine(line)
+            if re:
+                self.view.window().open_file(filename, sublime.ENCODED_POSITION)
+                # print("filename", filename)
+
+    def parseLuaError(self, line):
+        key = ".lua:"
+        posKeyStart = line.find(key)
+        if posKeyStart == -1:
+            return False,""
+        posKeyEnd = posKeyStart + len(key)
+        filename = line[:posKeyStart].strip(" \t")+".lua"
+        fileline = line[posKeyEnd:line.find(":",posKeyEnd)]
+
+        return True, filename+":"+fileline
+
+    def parseCocosErrorAndDump(self, line):
+        filename = ""
+        fileline = 1
+        keyHead = "[string \""
+        posHeadStart = line.find(keyHead)
+        if posHeadStart == -1:
+            return False,""
+
+        posHeadEnd = posHeadStart + len(keyHead)
+
+        key = ".lua\"]:"
+        posKeyStart = line.find(key,posHeadStart)
+        
+        if posHeadStart == -1:
+            return False,""
+
+        posKeyEnd = posKeyStart + len(key)
+
+        filename = line[posHeadEnd:posKeyStart]+".lua"
+        fileline = line[posKeyEnd:line.find(":",posKeyEnd)]
+
+        return True, filename+":"+fileline
+
+    def parseLine(self, line):
+        re,string = self.parseCocosErrorAndDump(line)
+        if re:
+            return re,string
+
+        return self.parseLuaError(line)
 
 # st3
 def plugin_loaded():
